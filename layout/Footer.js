2e1351d8a64fab15e2937221826bc52c9a7df9e4
Vince Lu <vincentlu@hotmail.ca>
2022-02-27 01:49:34 -0500

    updated footer with links to blog, policy, + terms

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
 const Footer = () => {
-  return <footer>Copyright 2022 recreate.ai</footer>;
+  return (
+    <>
+      <nav>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+        <Link href="/privacypolicy">
+          <a>Privacy Policy</a>
+        </Link>
+        <Link href="/termsofuse/">
+          <a>Terms of Use</a>
+        </Link>
+      </nav>
+      <footer>Copyright 2022 recreate.ai</footer>
+    </>
+  );
 };
 
 export default Footer;
