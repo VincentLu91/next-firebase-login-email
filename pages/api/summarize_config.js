99f571610d3535978871cfd523ca53e0ef6ee988
Vince Lu <vincentlu@hotmail.ca>
2022-03-04 15:16:23 -0500

    updated new summary endpoint with text as param

--- pages/api/summarize_config.js
+++ pages/api/summarize_config.js
@@ -1,8 +1,10 @@
 // this pertains to trainML
 
 const summarize_config = {
-  api_address: 'https://8815bb21-77af-4f2f-b0c6-06911e1e17a5.trainml.cloud',
-  route_path: '/summarize',
+  //api_address: "https://8815bb21-77af-4f2f-b0c6-06911e1e17a5.trainml.cloud",
+  api_address: "https://3d9145ca-6e2b-4074-b82f-71992a334d14.trainml.cloud",
+
+  route_path: "/summarize",
 };
 
 export default summarize_config;
