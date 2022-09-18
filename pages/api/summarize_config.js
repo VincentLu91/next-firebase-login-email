9a82930ad05d57abbaaac69e7f90c2104e6fa272
Vince Lu <vincentlu@hotmail.ca>
2022-09-17 22:59:16 -0400

    tried new summary API that works on all text sofar

--- pages/api/summarize_config.js
+++ pages/api/summarize_config.js
@@ -2,7 +2,7 @@
 
 const summarize_config = {
   //api_address: "https://8815bb21-77af-4f2f-b0c6-06911e1e17a5.trainml.cloud",
-  api_address: "https://3d9145ca-6e2b-4074-b82f-71992a334d14.trainml.cloud",
+  api_address: "https://1c2ca4f1-b44e-4547-891b-d7f035cae3b4.trainml.cloud",
 
   route_path: "/summarize",
 };
