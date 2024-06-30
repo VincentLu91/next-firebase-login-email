c0df8a1bd874e93ae639291dc1c9b4fa79ea66ef
Vince Lu <vincentlu@hotmail.ca>
2024-06-29 20:34:36 -0400

    navbar to conditionally include signin/signout links

--- layout/Navbar.js
+++ layout/Navbar.js
@@ -1,23 +1,34 @@
 import Link from "next/link";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useDispatch } from "react-redux";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const user = useUser();
+  const supabase = useSupabaseClient();
   return (
     <nav>
       <div className="logo">
         <h1>EchoAlly</h1>
       </div>
-      <Link href="/">
-        Home
-      </Link>
-      <Link href="/about">
-        About
-      </Link>
-      <Link href="/pricing">
-        Pricing
-      </Link>
-      <Link href="/signin/">
-        Sign In
-      </Link>
+      <Link href="/dashboard">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/pricing">Pricing</Link>
+      {user ? (
+        <a
+          href="#"
+          onClick={() => {
+            //signOut(auth);
+            supabase.auth.signOut(); // had to call this twice for some reason
+            //dispatch(setSound(null));
+            dispatch({ type: "SIGNED_OUT" });
+          }}
+        >
+          Sign Out
+        </a>
+      ) : (
+        <Link href="/signin/">Sign In</Link>
+      )}
     </nav>
   );
 };
