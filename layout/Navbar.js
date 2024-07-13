01743bdbb2eebb51ffad5398d64772e21eea6b6b
Vince Lu <vincentlu@hotmail.ca>
2024-07-13 19:54:49 -0400

    hang up call and decrement # of calls when a user signs out

--- layout/Navbar.js
+++ layout/Navbar.js
@@ -1,11 +1,35 @@
 import Link from "next/link";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { useCallback, useState, useEffect } from "react";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const user = useUser();
   const supabase = useSupabaseClient();
+  const [customer, setCustomer] = useState(null);
+  const callControlID = useSelector(
+    (state) => state.recordingReducer.callControlID
+  );
+  const getCustomerInfo = useCallback(
+    async (user) => {
+      if (user) {
+        let customerInfo = await supabase
+          .from("customers")
+          .select("*")
+          .eq("email_address", user.email);
+        console.log("customerInfo is: ", customerInfo.data[0]); //customerInfo.data[0].id
+        setCustomer(customerInfo.data[0]);
+      }
+    },
+    [supabase]
+  );
+  useEffect(() => {
+    //console.log("Current user is: ", currentUser);
+    getCustomerInfo(user);
+    //getSubscriptionsInfo();
+  }, [getCustomerInfo, user]);
   return (
     <nav>
       <div className="logo">
@@ -27,6 +51,11 @@ const Navbar = () => {
           <a
             href="#"
             onClick={() => {
+              if (callControlID) {
+                console.log("upon signout, callControlID is: ", callControlID);
+                axios.post(`/api/hangup?callControlID=${callControlID}`);
+                axios.post(`/api/calls-token?user=${customer.id}`);
+              }
               //signOut(auth);
               supabase.auth.signOut(); // had to call this twice for some reason
               //dispatch(setSound(null));
