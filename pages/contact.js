987d00421f17f2d2b24129871325bbc5d28b3456
Vince Lu <vincentlu@hotmail.ca>
2025-09-12 10:30:40 -0400

    added tabs to header

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+const contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>If you have any questions, You can contact us:</p>
+      <ul>
+        <li>By email: placeholder</li>
+      </ul>
+    </div>
+  );
+};
+
+export default contact;
